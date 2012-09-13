ad93552a40645e0101ee678ec6f246942b873933
Mattias Svedhem <mattias.svedhem@hotmail.com>
2012-09-13 15:49:44 +0200

    Remove require 'rubygems'.
    http://tomayko.com/writings/require-rubygems-antipattern

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
+# FIXME: We should modify the load path here.
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-require 'rubygems'
 
 require 'mongoid'
 require 'trackoid'
